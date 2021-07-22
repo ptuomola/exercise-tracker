97a735ef4e0213180dec94d74464c010ae2fe384
Petri Tuomola <petri@tuomola.org>
2021-07-23 07:11:32 +0800

    Fixing heroku postgresql issue

--- db.py
+++ db.py
@@ -1,6 +1,13 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import re
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+# Work around issue with Heroku Postgres and SQLAlchemy 1.4.x
+uri = getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 db = SQLAlchemy(app)
