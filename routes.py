68efb9cbc192b60f96dd6c9a066698e642415511
Petri Tuomola <petri@tuomola.org>
2021-07-25 15:47:07 +0800

    Fixing linting and starting to implement login

--- routes.py
+++
@@ -1,9 +0,0 @@
-from app import app
-import visits
-from flask import render_template
-
-@app.route("/")
-def index():
-    visits.add_visit()
-    counter = visits.get_counter()
-    return render_template("index.html", counter=counter)
--- tracker/main.py
+++ tracker/main.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
 @main.route('/profile')
 def profile():
-    return 'Profile'
+    return render_template('profile.html')
