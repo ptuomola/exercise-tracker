ebb7adbdd9c43b8a273cc31fef70b4e438940a23
Petri Tuomola <petri@tuomola.org>
2021-07-27 08:52:45 +0800

    Completing signup form with validations

--- tracker/auth.py
+++ tracker/auth.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from model.user import get_user_by_email, insert_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user
-from flask_wtf import Form 
+from flask_wtf import FlaskForm 
 from wtforms.fields import SubmitField, StringField, PasswordField
-from wtforms.validators import Required, Email, Length, EqualTo
+from wtforms.validators import Required, Email, Length, EqualTo, ValidationError, Regexp
 
 auth = Blueprint('auth', __name__)
 
@@ -27,15 +27,27 @@ def login_post():
     login_user(user, remember=remember)
     return redirect(url_for("main.profile"))
 
-class RegistrationForm(Form):
+class RegistrationForm(FlaskForm):
     name = StringField('Name', [Required(), Length(min=4, max=40)])
     email = StringField('Email address', [Required(), Email()])
     password = PasswordField('Password', [
         Required(), 
-        EqualTo('confirm', message='Passwords must match')
+        EqualTo('confirm', message='Passwords must match'),
+        Length(min=8, message="Password be at least 8 characters"),
+        Regexp("^(?=.*[a-z])", message="Password must have a lowercase character"),
+        Regexp("^(?=.*[A-Z])", message="Password must have an uppercase character"),
+        Regexp("^(?=.*\\d)", message="Password must contain a number"),
+        Regexp(
+            "(?=.*[@$!%*#?&])", message="Password must contain a special character"
+        ),
     ])
     confirm = PasswordField('Repeat password')
     submit = SubmitField('Signup')
+
+    def validate_email(form, field):
+        if get_user_by_email(field.data): 
+            raise ValidationError("Email address already exists")
+
 
 @auth.route('/signup')
 def signup():
@@ -47,10 +59,6 @@ def signup_post():
     form = RegistrationForm(request.form)
 
     if not form.validate():
-        return render_template("signup.html", form = form)
-
-    if get_user_by_email(form.email.data): 
-        flash("Email address already exists")
         return render_template("signup.html", form = form)
 
     insert_user(form.email.data, form.name.data, generate_password_hash(form.password.data, method='sha256'))
