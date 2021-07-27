e725362b3385fd9830114afe03a4773dff96fe0f
Petri Tuomola <petri@tuomola.org>
2021-07-27 08:40:33 +0800

    Menu to show options based on logged in status

--- tracker/nav.py
+++ tracker/nav.py
@@ -1,12 +1,21 @@
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
+from flask_login import current_user
 
 nav = Nav()
 
-nav.register_element("main", Navbar(
-    View("Home", "main.index"), 
-    View("Profile", "main.profile"), 
-    View("Login", "auth.login"), 
-    View("Sign up", "auth.signup"), 
-    View("Logout", "auth.logout")
-))
+def main_nav():
+    if current_user.is_authenticated:
+        return Navbar(
+            View("Home", "main.index"), 
+            View("Profile", "main.profile"), 
+            View("Logout", "auth.logout")
+        )
+    else:
+        return Navbar(
+            View("Home", "main.index"), 
+            View("Login", "auth.login"), 
+            View("Sign up", "auth.signup"), 
+        )
+
+nav.register_element("main_nav", main_nav)
